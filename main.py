5fd317528ce56616635c066445695ead8609b586
Eric Perreault <69309175+erperreault@users.noreply.github.com>
2021-08-19 15:05:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,3 @@ def testSuite(x):
         pprint(readCells(roastLogID, f'{getTabNames(roastLogID)[0]}!F16:F43'))
 
 populateBatches(roastLogID, 1, getRoastNumbers(roastCalcID))
-
-# Make tabs in roastLog with correct dates
-# Make dict of active coffee names and their template addresses ('Decaf [25]':'Template!F16')
-#TODO Fill tabs in roastLog with batches
-#TODO Get needed amounts from readShopify.
-#TODO Send roast schedule to Cropster.
-#TODO Make a GUI for the thing.
-#TODO Bundle it and throw it on GitHub.
-#TODO Make universal / executable / log-in-able ?
